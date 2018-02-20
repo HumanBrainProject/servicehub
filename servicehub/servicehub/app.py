38a2d01f1ebd8188065bc14ce6a5ca443a2271f7
Jonathan Villemaire-Krajden <jonathan.villemaire-krajden@epfl.ch>
2018-02-20 12:26:10 +0100

    Some creation logic in place.

--- servicehub/servicehub/app.py
+++ servicehub/servicehub/app.py
@@ -1,35 +1,80 @@
 # coding: utf-8
+import time
+
 
 import docker
 import tornado.ioloop
 import tornado.web
 
+# Number of seconds of inactivity before killing the container
 SESSION_LENGTH = 15*60
+# Number of seconds before assuming we failed to start the container
+STARTUP_MAX_TIME = 60
+
+def _(text):
+    '''Dummy gettext'''
+    return text
 
 
 class ServiceHubHandler(tornado.web.RequestHandler):
-    def initialize(self, sessions):
-        self.sessions = sessions
-
+#    @tornado.web.asynchronous
     def get(self):
         userid = self.request.headers['userid']
-        if userid in self.sessions:
-            self.write("session already exists\n")
+        sessions = self.application.user_sessions
+
+        try:
+            container = sessions[userid]
+        except KeyError:
+            self.start_container(userid)
+            self.write(str(sessions[userid]) + "\n")
+            self.set_header('refresh', '1; ' + self.request.uri)
             return
-        # @todo main queue for creation to avoid multiple simultaneous logins
+
+        if container is None:
+            self.set_header('refresh', '5; ' + self.request.uri)
+            self.write(_("We are launching your application, please be patient"))
+        elif isinstance(container, int):
+            if time.time() - container > STARTUP_MAX_TIME:
+                # @TODO
+                self.write(_("session failed to start\n"))
+            else:
+                self.set_header('refresh', '5; ' + self.request.uri)
+                self.write(_("We are launching your application, please be patient"))
+        elif container.status in ('running', 'created'):
+            self.write(_("Error: Your container is running, what are you doing here?"))
+            # @TODO what to do?
+        elif container.status == 'exited':
+            # Don't do anything, we'll create a new container
+            self.write(_("Your container had exited, started a new on.?"))
+            del sessions[userid]
+            self.set_header('refresh', '1; ' + self.request.uri)
+        else:
+            # @TODO what?
+            print("error: ")
+            print(container)
+            print(container.status)
+        return
+
+    def start_container(self, userid):
+        sessions = self.application.user_sessions
         client = docker.from_env()
-        self.sessions[userid] = client.containers.run("emilevauge/whoami", detach=True, remove=True)
-        self.write(str(self.sessions[userid]) +"\n")
-        self.set_header('refresh', '5; ' + self.request.url)
+        sessions[userid] = None
+        sessions[userid] = time.time()
+        sessions[userid] = client.containers.run("emilevauge/whoami", detach=True, remove=True)
 
-
-sessions = {}
+class ServiceHubApplication(tornado.web.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # TODO: I think this is threadsafe assuming tornado doesn't
+        # pause thread execution at wrong moment. otherwise, use
+        # something else then dictionary.
+        self.user_sessions = {}
 
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", ServiceHubHandler, {'sessions': {}}),
-    ])
+    return ServiceHubApplication([
+        (r"/", ServiceHubHandler),
+    ], autoreload=True)
 
 
 # @TODO initialize and clean docker images
